a2c18389a1a739b152f473ee699589c8e85ca75a
Dmiriy V.Simonov <ceo@birka-team.uk>
2025-06-04 12:57:14 +0300

    Удаляем неиспользуемый код

--- lib/GPBExim/View.pm
+++ lib/GPBExim/View.pm
@@ -25,59 +25,6 @@ sub new {
     return $self;
 }
 
-sub handle_request {
-    my $self = shift;
-    my $r = shift;
-    my %args = @_;
-
-    my $m = $self->{model};
-    my $return = { data => {} };
-
-    my ($method, $path, $content) = ($r->method, $r->uri->path, $r->content);
-
-    if ($method eq 'GET' && $path eq "/") {
-        $return = $self->root($r, $m);
-    } elsif ($method eq 'POST' && $path eq "/search") {
-        $return = $self->search($r, $m);
-    } elsif ($method eq 'POST' && $path eq "/suggest") {
-        $return = $self->suggest($r, $m);
-    } elsif (!$args{testit}) {
-        $return = { render => 'HTTP::Response', data => HTTP::Response->new(RC_NOT_FOUND) };
-    }
-
-    # вернём данные, если находимся в режиме теста
-    $args{testit} && return { data => $return->{data} };
-
-    # если пришёл готовый HTTP::Response, просто его возвращаем
-    if ($return->{render} eq 'HTTP::Response') {
-        return $return->{data};
-    }
-
-    # рендер через Template::Toolkit
-    if ($return->{render} eq 'TT') {
-        my $tt = Template->new(TRIM => 1, ABSOLUTE => 1);
-        my $body = '';
-        $tt->process( $return->{template}, $return, \$body )
-            or die $tt->error();
-
-        my $resp = HTTP::Response->new(RC_OK, undef, undef, $body);
-        $resp->header('Content-Type' => 'text/html; charset=utf-8');
-        return $resp;
-    }
-
-    # рендер JSON
-    if ($return->{render} eq 'JSON') {
-        my $body = encode_json($return);
-
-        my $resp = HTTP::Response->new(RC_OK, undef, undef, encode('UTF-8', $body));
-        $resp->header('Content-Type' => 'application/json; charset=utf-8');
-        return $resp;
-    }
-
-    # возвращаем просто данные, если дошли до этой строки
-    return { data => $return->{data} };
-}
-
 sub render {
     my $self = shift;
     my $data = shift;
