a65784748a68f9a732fb1e66a7eaafebeb8249b9
Dmiriy V.Simonov <ceo@birka-team.uk>
2025-06-09 16:31:21 +0300

    small fix

--- bin/testconn.pl
+++ bin/testconn.pl
@@ -8,9 +8,8 @@ use GPBExim::Log;
 use Cwd qw(abs_path);
 
 my $cfg = GPBExim::Config->get();
-my $model;
-my $parser;
 
 log(debug => "Get model %s", $cfg->{db}{model_type});
-$model = GPBExim::get_model($cfg->{db}{model_type});
+log(info => "Checking DB cred ", {$cfg->{db}{model_type}=>$cfg->{db}});
+GPBExim::get_model($cfg->{db}{model_type});
 
