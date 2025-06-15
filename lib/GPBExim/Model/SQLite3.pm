0311de529180ffd3fdf1c4b04ee99e0d8d2f7e39
Dmiriy V.Simonov <ceo@birka-team.uk>
2025-06-15 15:00:33 +0300

    Восстановим совместимость с SQLIte3

--- lib/GPBExim/Model/SQLite3.pm
+++ lib/GPBExim/Model/SQLite3.pm
@@ -4,6 +4,8 @@ use lib::abs '../../../lib';
 use uni::perl ':dumper';
 use parent 'GPBExim::Model';
 
+use GPBExim::Log;
+
 sub init {
     my $self = shift;
 
@@ -17,6 +19,19 @@ sub setup_dbh {
     my $self = shift;
     $self->{dbh} = DBI->connect(qq{dbi:SQLite:dbname=}.$self->{path}, "", "", { RaiseError => 1 });
 }
+
+sub clear_all_tables {
+    my $self = shift;
+
+    log(debug => 'SQLite3 clearing all tables');
+
+    $self->{dbh}->do('PRAGMA foreign_keys = OFF');
+    for my $table (map {@$_} @{$self->{dbh}->selectall_arrayref(q{SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%'})}) {
+        $self->{dbh}->do("DROP TABLE IF EXISTS \"$table\"");
+    }
+    $self->{dbh}->do('PRAGMA foreign_keys = ON');
+}
+
 
 sub sql_order_str { shift; my $field = shift; return sprintf('%s COLLATE BINARY', $field) }
 
